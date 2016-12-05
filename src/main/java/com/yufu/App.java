68f877f7d5d65c542778479119427d9ea583ad66
zjc1985 <lionhart586@hotmail.com>
2016-12-05 12:43:58 +0800

    First Commit
    first commit

---
+++ src/main/java/com/yufu/App.java
@@ -0,0 +1,12 @@
+package com.yufu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		SomeHelper helper=new SomeHelper();
+		System.out.println(helper.callHelloWorld());
+	}
+}
---
+++ src/main/java/com/yufu/SomeHelper.java
@@ -0,0 +1,7 @@
+package com.yufu;
+
+public class SomeHelper {
+	public String callHelloWorld() {
+		return "Hello World";
+	}
+}
